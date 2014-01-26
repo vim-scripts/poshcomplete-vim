2e0b10282b87ace629fa227329b32f379983b624
cd01 <cd01.jpn@gmail.com>
2014-01-26 00:00:00 +0000

    Version 0.1: Initial upload

---
+++ autoload/poshcomplete.vim
@@ -0,0 +1,30 @@
+let s:script_path = fnamemodify(resolve(expand('<sfile>:p')), ':h')
+
+function! poshcomplete#CompleteCommand(findstart, base)
+  if a:findstart
+    let line = getline('.')
+    let start = col('.') - 1
+
+    while start > 0 && (line[start - 1] =~ '\a' || line[start - 1] =~ '-')
+      let start -= 1
+    endwhile
+
+    return start
+  else
+    let currentline = getline('.') . substitute(a:base, "[0]$", "-", "") " Bacause dash is replaced by zero
+    let res = []
+
+    if currentline == ''
+        return []
+    endif
+    
+    let completionwords = eval(system("powershell -NoProfile -ExecutionPolicy unrestricted -Command \"" . fnameescape(s:script_path)  . "\\..\\completions.ps1 '" . currentline . "'\""))
+
+    for completionword in completionwords
+      call add(res, completionword)
+    endfor
+
+    return res
+  endif
+endfunction
+
---
+++ completions.ps1
@@ -0,0 +1,22 @@
+﻿
+
+if ($args[0] -ne $null) {
+    $inputText = $args[0].ToString()
+
+    Write-Host "[" -NoNewline
+
+    # better to sort?
+    $completeWords = [System.Management.Automation.CommandCompletion]::CompleteInput($inputText, $inputText.Length, $null).CompletionMatches | Select-Object CompletionText, ResultType, ToolTip
+    $cntCompleteWords = $completeWords.Count
+
+    foreach ($completeWord in $completeWords)
+    {
+        Write-Host "{" -NoNewline
+        Write-Host ("`"word`": `"" + $($completeWord.CompletionText -replace "\\","\\") + "`", ") -NoNewline 
+        Write-Host ("`"kind`": `"[" + $completeWord.ResultType + "]`", ") -NoNewline # must convert resulttype to single character?
+        Write-Host ("`"menu`": `"" + $($($($completeWord.ToolTip -replace "\\","\\") -replace "^`r`n","") -replace "`r`n"," ") + "`"") -NoNewline 
+        Write-Host "}," -NoNewline
+    }
+
+    Write-Host "]" -NoNewline
+}
