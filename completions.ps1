3b841d82c91b1603778d64f3c333cbc09b55ef93
cd01 <cd01.jpn@gmail.com>
2014-06-23 00:00:00 +0000

    Version 0.2: Next version will be require Python.

--- completions.ps1
+++ completions.ps1
@@ -1,22 +1,21 @@
-﻿
-
-if ($args[0] -ne $null) {
+﻿if ($args[0] -ne $null) {
     $inputText = $args[0].ToString()
 
     Write-Host "[" -NoNewline
 
-    # better to sort?
     $completeWords = [System.Management.Automation.CommandCompletion]::CompleteInput($inputText, $inputText.Length, $null).CompletionMatches | Select-Object CompletionText, ResultType, ToolTip
-    $cntCompleteWords = $completeWords.Count
 
     foreach ($completeWord in $completeWords)
     {
         Write-Host "{" -NoNewline
         Write-Host ("`"word`": `"" + $($completeWord.CompletionText -replace "\\","\\") + "`", ") -NoNewline 
-        Write-Host ("`"kind`": `"[" + $completeWord.ResultType + "]`", ") -NoNewline # must convert resulttype to single character?
+        Write-Host ("`"kind`": `"[" + $completeWord.ResultType + "]`", ") -NoNewline
         Write-Host ("`"menu`": `"" + $($($($completeWord.ToolTip -replace "\\","\\") -replace "^`r`n","") -replace "`r`n"," ") + "`"") -NoNewline 
         Write-Host "}," -NoNewline
     }
 
     Write-Host "]" -NoNewline
 }
+
+# vim:set et ts=4 sts=0 sw=4 ff=dos:
+
